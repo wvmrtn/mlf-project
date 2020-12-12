44a27c0fa272cbedb0dee485b78de9294a103852
wvmrtn <william.vincent.martin@gmail.com>
2020-12-12 12:38:26 +0100

    changed library name

---
+++ mlf/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Created on Fri Dec 11 15:45:24 2020.
+
+@author: williammartin
+"""
+
+# import standard libraries
+# import third-party libraries
+# import local libraries
+from .config import *
+from .patent_utils import *
+
+if __name__ == '__main__':
+    pass
---
+++ mlf/config.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Created on Fri Dec 11 19:29:34 2020.
+
+@author: williammartin
+"""
+
+# import standard libraries
+# import third-party libraries
+# import local libraries
+
+TICK_MAP = {'Bristol-Myers Squibb': 'BMY',
+            'Celgene': None,  # private company
+            'Vertex': 'VRTX',
+            'Gilead': 'GILD',
+            'Allergan': None,  # private?
+            'Roche': 'RO.SW',
+            'Amgen': 'AMGN',
+            'Johnson & Johnson': 'JNJ',
+            'Novo Nordisk': 'NOVO-B',
+            'AbbVie': 'ABBV',
+            'Pfizer': 'PFE',
+            'AstraZeneca': 'AZN',
+            'Biogen Idec': 'BIIB',
+            'Shire':,
+            'Sanofi':,
+            'Merck':,
+            'GlaxoSmithKline':,
+            'Novartis':,
+            'Regeneron':,
+            'Bayer':,
+            'Eli Lilly':,
+            'Alexion':}
+
+if __name__ == '__main__':
+    pass
