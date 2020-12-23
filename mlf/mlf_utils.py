572922ee12bc64c651a70798dbf662be812b438d
wvmrtn <william.vincent.martin@gmail.com>
2020-12-23 15:04:30 +0100

    tested linear regression: poor results

--- mlf/mlf_utils.py
+++ mlf/mlf_utils.py
@@ -14,128 +14,157 @@ import numpy as np
 import gensim
 from gensim import corpora
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 # import local libraries
 
 
-def generate_xy(num_topics, num_days, keep_app=True, delay=60):
-    """Generate X and y for machine learning model.
+class LinearRegressor(LinearRegression):
 
-    Parameters
-    ----------
-    num_topics : int
-        Number of topics to parse.
-    num_days : int
-        Number of days to predict.
-    keep_app : bool, optional
-        Keep only application dates, otherwise granted. The default is True.
-    delay : int, optional
-        Number days to consider when computing feature. The default is 60.
+    def __init__(self):
+        super().__init__()
+        self.X = pd.DataFrame()
+        self.X_info = pd.DataFrame()
+        self.y = pd.DataFrame()
+        self.X_scaled = pd.DataFrame()
+        self.scaler = None
 
-    Returns
-    -------
-    X : pd.DataFrame
-        DataFrame containing features.
-    y : pd.DataFrame
-        DataFrame containing labels.
+    def scale_x(self, scaler=StandardScaler):
+        self.scaler = scaler()
+        self.scaler = self.scaler.fit(self.X)
+        X_scaled = self.scaler.transform(self.X)
+        self.X_scaled = pd.DataFrame(data=X_scaled, index=self.X.index,
+                                     columns=self.X.columns)
+        return self.X_scaled
 
-    """
-    # get available topics
-    NUM_TOPICS = glob.glob(r'data/nlp/*.gensim')
-    NUM_TOPICS = [int(re.findall(r'\d+', s)[0]) for s in NUM_TOPICS]
-    NUM_DAYS = list(range(2, 32))
+    def generate_xy(self, num_topics, num_days, keep_app=True, delay=60):
+        """Generate X and y for machine learning model.
 
-    assert num_topics in\
-        NUM_TOPICS, f'LDA model not found for {num_topics} topics'
-    assert num_days in\
-        NUM_DAYS, f'{num_days} outside scope of admissible days'
+        Parameters
+        ----------
+        num_topics : int
+            Number of topics to parse.
+        num_days : int
+            Number of days to predict.
+        keep_app : bool, optional
+            Keep only application dates, otherwise grant. The default is True.
+        delay : int, optional
+            Number days to consider when computing feature. The default is 60.
+        scaler : sklearn.preprocessing.OBJ, optional
+            Scaler of sklearn.preprocessing to use to scaler the data.
 
-    # create input and output of models
-    # load patent data
-    patents = pd.read_csv('data/patents/clean/patents.csv.gz',
-                          compression='gzip')
-    patents['text'] = patents['text'].apply(lambda x: x.split(' '))
+        Returns
+        -------
+        X : pd.DataFrame
+            DataFrame containing features.
+        y : pd.DataFrame
+            DataFrame containing labels.
 
-    # load returns
-    stocks = pd.read_csv('data/returns/clean/adj_stock_returns.csv',
-                         index_col=0)
-    stocks.index = pd.to_datetime(stocks.index)
+        """
+        # get available topics
+        NUM_TOPICS = glob.glob(r'data/nlp/*.gensim')
+        NUM_TOPICS = [int(re.findall(r'\d+', s)[0]) for s in NUM_TOPICS]
+        NUM_DAYS = list(range(2, 31))
 
-    # load lda model
-    ldamodel = gensim.models.ldamodel.LdaModel.load(
-        f'data/nlp/model{num_topics}.gensim')
+        assert num_topics in\
+            NUM_TOPICS, f'LDA model not found for {num_topics} topics'
+        assert num_days in\
+            NUM_DAYS, f'{num_days} outside scope of admissible days'
 
-    # remove columns from patents
-    keep_col = ['app_number', 'cited_patent_number', 'num_inventor',
-                'patent_num_claims', 'ticker', 'text',
-                'app_date', 'patent_date']
-    X = patents[keep_col]
+        # create input and output of models
+        # load patent data
+        patents = pd.read_csv('data/patents/clean/patents.csv.gz',
+                              compression='gzip')
+        patents['text'] = patents['text'].apply(lambda x: x.split(' '))
 
-    X.loc[:, 'app_date'] = pd.to_datetime(X.loc[:, 'app_date'])
-    X.loc[:, 'patent_date'] = pd.to_datetime(X.loc[:, 'patent_date'])
+        # load returns
+        stocks = pd.read_csv('data/returns/clean/adj_stock_returns.csv',
+                             index_col=0)
+        stocks.index = pd.to_datetime(stocks.index)
 
-    # create feature with number of patent applications filed
-    # in the last 60 days.
-    X['num_app_prior'] = np.nan
-    for ticker, x in X.groupby('ticker'):
-        for i, patent in x.iterrows():
-            app_date_delayed = patent['app_date'] - timedelta(days=delay)
-            num_app_prior = len(x[(x['app_date'] >= app_date_delayed) &
-                                  (x['app_date'] < patent['app_date'])]
-                                )
-            X.at[i, 'num_app_prior'] = num_app_prior
+        # load lda model
+        ldamodel = gensim.models.ldamodel.LdaModel.load(
+            f'data/nlp/model{num_topics}.gensim')
 
-    date_col = ''
-    remove = ''
-    if keep_app:
-        date_col = 'app_date'
-        remove = 'patent_date'
-    else:
-        date_col = 'patent_date'
-        remove = 'app_date'
-    # rename to date and remove column that we don't need
-    X = X.drop(columns=[remove])
-    X = X.rename(columns={date_col: 'date'})
-    X = X.sort_values(by='date')
+        # remove columns from patents
+        keep_col = ['app_number', 'cited_patent_number', 'num_inventor',
+                    'patent_num_claims', 'ticker', 'text',
+                    'app_date', 'patent_date']
+        X = patents[keep_col]
 
-    # create corpora and dictionary
-    dictionary = corpora.Dictionary(list(patents['text'].values))
+        X.loc[:, 'app_date'] = pd.to_datetime(X.loc[:, 'app_date'])
+        X.loc[:, 'patent_date'] = pd.to_datetime(X.loc[:, 'patent_date'])
 
-    # create feature with topic extraction from lda modela
-    X[[f'topic{t}' for t in range(num_topics)]] = 0.0
+        # create feature with number of patent applications filed
+        # in the last 60 days.
+        X['num_app_prior'] = np.nan
+        for ticker, x in X.groupby('ticker'):
+            for i, patent in x.iterrows():
+                app_date_delayed = patent['app_date'] - timedelta(days=delay)
+                num_app_prior = len(x[(x['app_date'] >= app_date_delayed) &
+                                      (x['app_date'] < patent['app_date'])]
+                                    )
+                X.at[i, 'num_app_prior'] = num_app_prior
 
-    for i, x in X.iterrows():
-        new_doc_bow = dictionary.doc2bow(x['text'])
-        t_prob = ldamodel.get_document_topics(new_doc_bow,
-                                              minimum_probability=0.0)
-        for t in t_prob:
-            X.at[i, 'topic'+str(t[0])] = t[1]
+        date_col = ''
+        remove = ''
+        if keep_app:
+            date_col = 'app_date'
+            remove = 'patent_date'
+        else:
+            date_col = 'patent_date'
+            remove = 'app_date'
+        # rename to date and remove column that we don't need
+        X = X.drop(columns=[remove])
+        X = X.rename(columns={date_col: 'date'})
+        X = X.sort_values(by='date')
 
-    # drop columns that are not needed
-    X = X.drop(columns=['text'])
+        # create corpora and dictionary
+        dictionary = corpora.Dictionary(list(patents['text'].values))
 
-    # remove all dates that are on 2010-01-04 (starting day of stocks)
-    # since we need to get 1 day of returns prior to date
-    X = X[X['date'] > '2010-01-04']
+        # create feature with topic extraction from lda modela
+        X[[f'topic{t}' for t in range(num_topics)]] = 0.0
 
-    # define y output
-    y = pd.DataFrame(index=X.index, columns=['y_ret', 'y_bin'], data=np.nan)
+        for i, x in X.iterrows():
+            new_doc_bow = dictionary.doc2bow(x['text'])
+            t_prob = ldamodel.get_document_topics(new_doc_bow,
+                                                  minimum_probability=0.0)
+            for t in t_prob:
+                X.at[i, 'topic'+str(t[0])] = t[1]
 
-    for i, x in X.iterrows():
-        # get returns one day before date
-        sub = stocks[x['ticker']]
-        ret_before = sub[sub.index < x['date']].iloc[-1:]
-        ret_after = sub[sub.index >= x['date']].iloc[:num_days-1]
-        ret = pd.concat([ret_before, ret_after], axis=0)
-        ret = (ret + 1).cumprod(axis=0).iloc[-1]-1
-        y.at[i, 'y_ret'] = ret
-        y.at[i, 'y_bin'] = 0 if ret < 0 else 1
+        # drop columns that are not needed
+        X = X.drop(columns=['text'])
 
-    # remove nans in y (some stocks were not publicly traded before some date)
-    index_drop = y[y['y_ret'].isna()].index
-    y = y.drop(index=index_drop)
-    X = X.drop(index=index_drop)
+        # remove all dates that are on 2010-01-04 (starting day of stocks)
+        # since we need to get 1 day of returns prior to date
+        X = X[X['date'] > '2010-01-04']
 
-    return X, y
+        # define y output
+        y = pd.DataFrame(index=X.index, columns=['y_ret', 'y_bin'],
+                         data=np.nan)
+
+        for i, x in X.iterrows():
+            # get returns one day before date
+            sub = stocks[x['ticker']]
+            ret_before = sub[sub.index < x['date']].iloc[-1:]
+            ret_after = sub[sub.index >= x['date']].iloc[:num_days-1]
+            ret = pd.concat([ret_before, ret_after], axis=0)
+            ret = (ret + 1).cumprod(axis=0).iloc[-1]-1
+            y.at[i, 'y_ret'] = ret
+            y.at[i, 'y_bin'] = 0 if ret < 0 else 1
+
+        # remove nans in y (some stocks were not publicly traded before
+        # some date)
+        index_drop = y[y['y_ret'].isna()].index
+        y = y.drop(index=index_drop)
+        X = X.drop(index=index_drop)
+
+        # get X for model and X containing info of X
+        self.X_info = X[['app_number', 'ticker', 'date']]
+        self.X = X.drop(columns=['app_number', 'ticker', 'date'])
+        self.y = y
+
+        return X, y
 
 
 if __name__ == '__main__':
--- test_william.py
+++ test_william.py
@@ -7,9 +7,26 @@
 
 # import standard libraries
 # import third-party libraries
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest, f_regression
 # import local libraries
-from mlf import generate_xy
+from mlf import LinearRegressor
 
 if __name__ == '__main__':
-    
-    X, y = generate_xy(num_topics=20, num_days=7)
+
+    lr = LinearRegressor()
+    lr.generate_xy(num_topics=50, num_days=3)
+    lr.scale_x()
+
+    # sk = SelectKBest(score_func=f_regression, k=5)
+    # sk = sk.fit(lr.X_scaled, lr.y['y_bin'])
+    # X = sk.transform(lr.X_scaled)
+
+    # separate train and test
+    X_train, X_test, y_train, y_test = train_test_split(lr.X_scaled,
+                                                        lr.y['y_ret'],
+                                                        test_size=0.2)
+
+    reg = lr.fit(X_train, y_train)
+    print(reg.score(X_train, y_train))
+    print(reg.score(X_test, y_test))
